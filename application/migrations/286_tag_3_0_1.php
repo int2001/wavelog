95e0ab29aebd90c43f007a48fce9cc00b8f1e39f
Andreas Kristiansen <6977712+AndreasK79@users.noreply.github.com>
2026-07-07 09:32:52 +0200

    Added changes in Norwegian subdivisions

--- application/migrations/286_tag_3_0_1.php
+++ application/migrations/286_tag_3_0_1.php
@@ -26,8 +26,21 @@ class Migration_tag_3_0_1 extends CI_Migration {
 
         $table = $this->config->item('table_name');
         $this->db->query("ALTER TABLE `station_profile`
-           MODIFY `station_pota` varchar(255) DEFAULT NULL
+			MODIFY `station_pota` varchar(255) DEFAULT NULL
         ");
+
+		$this->dbtry("INSERT INTO primary_subdivisions (adif, state, subdivision, deprecated) VALUES
+		(266,'31','Østfold',0),
+		(266,'32','Akershus',0),
+		(266,'33','Buskerud',0),
+		(266,'39','Vestfold',0),
+		(266,'40','Telemark',0),
+		(266,'55','Troms',0),
+		(266,'56','Finnmark',0)");
+
+		$this->dbtry("delete from primary_subdivisions where adif = 266 and state = 54");
+		$this->dbtry("delete from primary_subdivisions where adif = 266 and state = 30");
+		$this->dbtry("delete from primary_subdivisions where adif = 266 and state = 38");
     }
 
     public function down()
@@ -36,6 +49,14 @@ class Migration_tag_3_0_1 extends CI_Migration {
         $this->db->update('options', array('option_value' => '3.0.0'));
 
         // Do not cut station_pota back down to 50
+    }
+
+	function dbtry($what) {
+        try {
+            $this->db->query($what);
+        } catch (Exception $e) {
+            log_message("error", "Error executing operations on primary_subdivisions: ".$e." // Executing: ".$this->db->last_query());
+        }
     }
 
 }
