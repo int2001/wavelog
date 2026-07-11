d126ef73049b17fe35b34633cef592401a0b2410
HB9HIL <fabian.berg@hb9hil.org>
2026-07-11 20:52:49 +0200

    more safe approach

--- application/migrations/228_rename_satellites.php
+++ application/migrations/228_rename_satellites.php
@@ -174,15 +174,16 @@ class Migration_rename_satellites extends CI_Migration {
 		$this->add_ix('TMP_HRD_IDX_COL_SAT_NAME','`COL_SAT_NAME`');
 		if ($this->db->table_exists('satellite')) {
 
-			$fields = array(
-				'displayname' => array(
-					'name' => 'exportname',
-					'type' => 'VARCHAR',
-					'constraint' => 255,
-				),
-			);
-
-			$this->dbforge->modify_column('satellite', $fields);
+			if ($this->db->field_exists('displayname', 'satellite')) {
+				$fields = array(
+					'displayname' => array(
+						'name' => 'exportname',
+						'type' => 'VARCHAR',
+						'constraint' => 255,
+					),
+				);
+				$this->dbforge->modify_column('satellite', $fields);
+			}
 
 			foreach ($this->satellites as $exportname => $name) {
 				$this->update_sat_table($name, $exportname);
